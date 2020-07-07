35036e3d55de52736d3ad5839fc3f2860d7ba028
Rowan Matulis <rmatulis@dgitsystems.com>
2020-07-07 10:35:43 +1000

    Update approve_and_merge_for_user.py

--- approve_and_merge_for_user.py
+++ approve_and_merge_for_user.py
@@ -75,41 +75,8 @@ if __name__ == '__main__':
 
     #load event data
     event_data = json.load(open(github_event_path))
+    
+    print(json.dumps(event_data, indent=4, sort_keys=True))
 
     sess = get_session(github_token)
     sess_personal = get_session_personal(github_personal_token)
-
-
-    pull_request = event_data["pull_requests"][0]
-    pr_number = pull_request["number"]
-    pr_src = pull_request["head"]["ref"]
-    pr_dst = pull_request["base"]["ref"]
-
-    print(f"*** Checking pull request #{pr_number}: {pr_src} ~> {pr_dst}")
-    pr_data = sess.get(pull_request["url"]).json()
-
-
-    #lets check who created the PR 
-    pr_user = pr_data["user"]["login"]
-    print(f"*** This PR was opened by {pr_user}")
-    if pr_user != github_pr_author:
-        print("*** This PR was opened by somebody who isn't {github_pr_author} !")
-        
-
-        #add a review to the pull request saying it shouldnt exist
-        review_url = pull_request["url"] + "/reviews"
-        review_json = {"body": "Stable branches do not accept pull requests other then from jenkins.", "event": "COMMENT"}
-        review_response = sess.post(review_url,json=review_json);
-
-        if review_response.status_code == 200:
-            print ('Review post made succesfully.')
-        else:
-            print ('Review post error')
-            sys.exit(1)
-
-        neutral_exit()
-
-    #this needs to be done under the personal token not the repo one so it works
-    print("*** This PR is ready to be merged.")
-    merge_url = pull_request["url"] + "/merge"
-    sess_personal.put(merge_url)
